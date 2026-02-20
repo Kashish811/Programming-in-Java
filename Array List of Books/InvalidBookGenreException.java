5622eccbb0a843f024b28ddd5b1353f448b7b99b
Kashish811 <kashish081106@gmail.com>
2026-02-20 11:42:57 +0530

    Delete Array List of Books directory

--- Array List of Books/InvalidBookGenreException.java
+++
@@ -1,5 +0,0 @@
-public class InvalidBookGenreException extends Exception {
-    public InvalidBookGenreException(String message) {
-        super(message);   //Calls the constructor of the Exception class
-    }
-}
--- Array List of Books/InvalidPriceException.java
+++
@@ -1,5 +0,0 @@
-public class InvalidPriceException extends Exception {
-    public InvalidPriceException(String message) {
-        super(message);   //Calls the constructor of the Exception class
-    }
-}
